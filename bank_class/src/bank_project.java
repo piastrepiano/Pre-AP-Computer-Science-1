e7dd7c94d9ec67d010e75fa64b126c05edbf1da3
piastrepiano <32347803+piastrepiano@users.noreply.github.com>
2018-04-22 17:35:39 -0500

    Delete bank_project.java

--- bank_class/src/bank_project.java
+++
@@ -1,8 +0,0 @@
-public class bank_project {
-    public static void main(String args[]){
-        Bank kim = new Bank();
-
-        kim.checkingDeposit(20.0);
-        System.out.print("Your current checking balance is: " + kim.getChecking());
-    }
-}
