db19a14b101ad025378c31163092b89d51981e9e
piastrepiano <32347803+piastrepiano@users.noreply.github.com>
2018-05-23 11:10:31 -0500

    Update Graphics_locations.java

--- Graphics_locations.java
+++ Graphics_locations.java
@@ -3,52 +3,38 @@ import java.applet.*;
 
 public class Graphics_locations extends Applet{
     public void paint(Graphics g){
-        draw.start(g);
+        draw.abandoned_house(g);
     }
 }
 
 class draw{
-    public static void start(Graphics g){
-        Color violet = new Color(25, 25, 112);
-        //bg
-        g.setColor(violet);
+    public static void abandoned_house(Graphics g){
+        //bg color
+        g.setColor(Color.green);
         g.fillRect(10,10, 1000,600);
-        //moon
-        g.setColor(Color.white);
-        g.fillArc(725, 50, 150, 150, 0, 360);
-        //clouds 1
-        g.setColor(Color.lightGray);
-        g.fillArc(650, 125,75, 75, 0, 360);
-        g.fillArc(700, 125, 75, 75, 0, 360);
-        g.fillArc(615, 160, 75, 75, 0, 360);
-        g.fillArc(650, 185, 75, 75, 0, 360);
-        g.fillArc(700, 185, 75, 75, 0, 360);
-        g.fillArc(735, 160, 75, 75, 0, 360);
-        g.fillArc(675, 160, 75, 75, 0, 360);
-        //clouds 2
-        g.fillArc(825, 125,75, 75, 0, 360);
-        g.fillArc(875, 125, 75, 75, 0, 360);
-        g.fillArc(790, 160, 75, 75, 0, 360);
-        g.fillArc(825, 185, 75, 75, 0, 360);
-        g.fillArc(875, 185, 75, 75, 0, 360);
-        g.fillArc(910, 160, 75, 75, 0, 360);
-        g.fillArc(850, 160, 75, 75, 0, 360);
-        
-        /*
-        // a darker shade of blue bottom part (21, 21, 94)
-        
-            tree color(98, 78, 44)
-            darker tree color(83, 49, 24)
-            
-            Random rndInt = new Random(12345);
-        for (int k = 1; k <= 10; k++) {
-            x1 = ((int)(Math.random() * 15 + 1000));
-            int w = ((int)(Math.random() * 30 + 50));
-            g.fillRect(x1, 255, w, 385);
-        for (int k = 1; k <= 10; k++) {
-            x1 = ((int)(Math.random() * 15 + 1000));
-            int w = ((int)(Math.random() * 30 + 50));
-            g.fillRect(x1, 270, w, 385);
-        */
+        //color of house
+        Color brown = new Color(100, 61, 1);
+        g.setColor(brown);
+        //base
+        g.fillRect(300, 300, 500, 310);
+        //top
+        g.setColor(Color.gray);
+        Polygon top = new Polygon();
+        top.addPoint(330,100);
+        top.addPoint(770,100);
+        top.addPoint(830, 300);
+        top.addPoint(270, 300);
+        g.fillPolygon(top);
+        //lines roof
+        g.setColor(Color.darkGray);
+        g.drawPolygon(top);
+        g.drawLine(325, 120, 775, 120);
+        g.drawLine(320, 140, 780, 140);
+        g.drawLine(310, 170, 790, 170);
+        g.drawLine(300, 200, 800, 200);
+        g.drawLine(290, 230, 810, 230);
+        g.drawLine(280, 260, 820, 260);
+
     }
 }
+//draw string 2 ints and a string x, y, literal string
