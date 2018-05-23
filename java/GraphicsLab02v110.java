55e52bf9d83979476a1ece4400ca435d7a58ef3b
piastrepiano <32347803+piastrepiano@users.noreply.github.com>
2018-05-23 11:23:55 -0500

    Update GraphicsLab02v110.java

--- java/GraphicsLab02v110.java
+++ java/GraphicsLab02v110.java
@@ -1,89 +1,61 @@
 import java.awt.*;
 import java.applet.*;
 
-public class GraphicsLab02v110 extends Applet{
-    public void paint(Graphics g){
-        //exterior rectangle
-        int width = 980;
-        int height = 630;
-        g.drawRect(10,10, width,height);
+public class Graphics_locations extends Applet {
+    public void paint(Graphics g) {
 
-        // bottom left corner
-        int x1 = 10;
-        int y1 = 10;
-        int x2 = 10;
-        for(int k =1; k < 44; k ++) {
-            g.drawLine(x1, y1, x2, height + 10);
-            y1 += 15;
-            x2 += 20;
-        }
-        // bottom right corner
-        y1 = 10;
-        x2 = 990;
-        for(int k = 1; k < 44; k ++){
-            g.drawLine(width + 10, y1, x2, height + 10);
-            y1 += 15;
-            x2 -= 20;
-        }
-        // top right corner
-        x1 = 10;
-        y1 = 640;
-        x2 = 10;
-        int y2 = 10;
-        for(int k =1; k < 44; k ++){
-            g.drawLine(x1, y1, x2, y2);
-            y1 -= 15;
-            x2 += 20;
-        }
-        //top left corner
-        y1 = 640;
-        x2 = 990;
-        y2 = 10;
-        for(int k =1; k < 44; k ++){
-            g.drawLine(width + 10, y1, x2, y2);
-            y1 -= 15;
-            x2 -= 20;
-        }
+    }
 
-        //interior rectangle
-        width = 620;
-        height = 270;
-        g.drawRect(190, 190, width, height);
-        // bottom left corner
-        x1 = 190;
-        y1 = 190;
-        x2 = 190;
-        for(int k =1; k < 26; k ++) {
-            g.drawLine(x1, y1, x2, height + 190);
-            y1 += 11;
-            x2 += 19;
-        }
-        // bottom right corner
-        y1 = 190;
-        x2 = 810;
-        for(int k = 1; k < 26; k ++){
-            g.drawLine(width + 190, y1, x2, height + 190);
-            y1 += 11;
-            x2 -= 19;
-        }
-        //top right corner
-        y1 = 460;
-        x2 = 810;
-        y2 = 190;
-        for(int k =1; k < 26; k ++) {
-            g.drawLine(width + 190, y1, x2, y2);
-            y1 -= 11;
-            x2 -= 19;
-        }
-        // top left corner
-        x1 = 190;
-        y1 = 460;
-        x2 = 190;
-        y2 = 190;
-        for(int k =1; k < 26; k ++) {
-            g.drawLine(x1, y1, x2, y2);
-            y1 -= 11;
-            x2 += 19;
+Class draw{ 
+        public static void abandoned_house (Graphics g){
+    }
+        public static void base (Graphics g){
+        //bg color
+        g.setColor(Color.green);
+        g.fillRect(10, 10, 1000, 600);
+        //color of house
+        Color brown = new Color(100, 61, 1);
+        g.setColor(brown);
+        //base
+        g.fillRect(300, 300, 500, 310);
+        //top
+        g.setColor(Color.gray);
+        Polygon top = new Polygon();
+        top.addPoint(330, 100);
+        top.addPoint(770, 100);
+        top.addPoint(830, 300);
+        top.addPoint(270, 300);
+        g.fillPolygon(top);
+    }
+
+        public static void roof_lines {
+            //lines roof
+            g.setColor(Color.darkGray);
+            g.drawPolygon(top);
+            g.drawLine(325, 120, 775, 120);
+            g.drawLine(320, 140, 780, 140);
+            g.drawLine(310, 170, 790, 170);
+            g.drawLine(300, 200, 800, 200);
+            g.drawLine(290, 230, 810, 230);
+            g.drawLine(280, 260, 820, 260);
+
+            int x1 = 330;
+            int x2 = 270;
+            for (int k = 1; k <= 7; k++) {
+                g.drawLine(x1, 100, x2, 300);
+                x1 += 30;
+                x2 += 30;
+            }
+
+            int x1_1 = 770;
+            int x2_2 = 830;
+            for (int k = 1; k <= 7; k++) {
+                g.drawLine(x1_1, 100, x2_2, 300);
+                x1_1 -= 30;
+                x2_2 -= 30;
+            }
         }
     }
+    
 }
+//draw string 2 ints and a string x, y, literal string
