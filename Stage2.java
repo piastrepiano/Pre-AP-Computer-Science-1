8567cf8cd21bb02b72d6da8946e73707cd17d907
piastrepiano <32347803+piastrepiano@users.noreply.github.com>
2018-05-30 11:07:43 -0500

    Update Stage2.java

--- Stage2.java
+++ Stage2.java
@@ -63,7 +63,7 @@ public class Stage2 {
         System.out.println("You must enter and go to the heart of the temple where the alter laysss.");
         System.out.println("You may encounter enemies as you go. To defeat them, you have to unlock the powersss from within.");
         System.out.println("This is possible by collecting orbs." +
-                "\nThere are 5 different types of orbs available in this temple: fire, water, psychic, wind, and ice;");
+                "\nThere are 5 different types of orbs available in this temple: fire, water, psychic, wind, and ice.");
         console.nextLine();
         System.out.println("hiss...hiss...My introduction is done. Let's go Massster.");
         console.nextLine();
@@ -81,7 +81,7 @@ public class Stage2 {
         System.out.println("My name is Chichwa and I'm here to assist you in your journey of saving Eliteless,");
         System.out.println("You must find the Amazon River and follow its trail towards the sun.");
         System.out.println("There will be enemies as well as orbs on your way. These orbs will help you defeat enemies and stay alive." +
-                "\nThe 5 different types of orbs available in this forest are poison, water, earth, wind, and bug;");
+                "\nThe 5 different types of orbs available in this forest are poison, water, earth, wind, and bug.");
         console.nextLine();
         System.out.println("OO OO AWW AWW!!! My introduction is done! Let's go Alpha!");
         console.nextLine();
@@ -90,21 +90,92 @@ public class Stage2 {
 
     public void direction(){
         Scanner lor = new Scanner(System.in);
-        System.out.print("\nWhat direction do you wish to go? (left, right)");
-        direction = lor.nextLine();
-        
+
         if(china){
-            china_maze();
+            System.out.print("\nWhat direction do you wish to go? (left, right)");
+            direction = lor.nextLine();
+
+            if(direction.equals("left")){
+                System.out.print("You turned left.");
+            }
+            else if(direction.equals("right")){
+                System.out.print("You turned right.");
+            }
+            else{
+                System.out.print("That is not a direction. Try again.");
+            }
         }
         else if(amazon_forest){
-            amazon_river_maze();
+            System.out.print("\nWhat direction do you wish to go? (left, right, straight)");
+            direction = lor.nextLine();
+
+            if(direction.equals("left")){
+                System.out.print("You turned left.");
+            }
+            else if(direction.equals("right")){
+                System.out.print("You turned right.");
+            }
+            else if(direction.equals("straight")){
+                System.out.print("You continued walking straight.");
+            }
+            else{
+                System.out.print("That is not a direction. Try again.");
+            }
         }
     }
-    public void china_maze(){
+    public void china_enemies(){
         //Enemies(dragon slayer, phoenix, nine tailed fox
+        Random percentage = new Random();
+        Integer dpns = percentage.nextInt(100) + 1;
+
+        if(dpns <= 30){
+            Random enemy = new Random();
+            Integer enemies = enemy.nextInt(3) + 1;
+
+            if(enemies.equals(1)){
+                //dragon slayer
+            }
+            else if(enemies.equals(2)){
+                //phoenix
+            }
+            else if(enemies.equals(3)){
+                //nine tail fox
+            }
+        }
+        else{
+            //safe
+        }
     }
 
-    public void amazon_river_maze(){
+    public void amazon_river_enemies(){
         //Enemies(dragon slayer, poachers, yeti
+        Random percentage = new Random();
+        Integer dpys = percentage.nextInt(100) + 1;
+
+        if(dpys <= 30){
+            Random enemy = new Random();
+            Integer enemies = enemy.nextInt(3) + 1;
+
+            if(enemies.equals(1)){
+                //dragon slayer
+            }
+            else if(enemies.equals(2)){
+                //poachers
+            }
+            else if(enemies.equals(3)){
+                //yeti
+            }
+        }
+        else{
+            //safe
+        }
+    }
+
+    public void china_orbs(){
+        //orbs: fire, water, psychic, wind, and ice
+    }
+
+    public void amazon_river_orbs(){
+        //orbs: poison, water, earth, wind, and bug
     }
 }
